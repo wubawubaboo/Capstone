494bd5703ee295733f3395cdbef2566b904afd7e
wubawubaboo <ludwig.jose26@gmail.com>
2026-07-28 16:27:10 +0800

    fixing resident module

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin'; // <-- This is the missing import
+import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.jsx', // Make sure this matches your entry file (app.jsx or app.tsx)
+            input: 'resources/js/app.jsx',
             refresh: true,
         }),
         react(),
